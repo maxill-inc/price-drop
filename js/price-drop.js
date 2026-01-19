8b2b7d892ff722d00bdf0de7107cf85c29e7262b
ks <124356972+kysch13@users.noreply.github.com>
2026-01-19 15:34:53 -0500

    Redo positioning of walls and triangles

--- js/price-drop.js
+++ js/price-drop.js
@@ -4,12 +4,13 @@
  * maxill.com
  */
 
-
 const priceDrop = {
     width: 400,
-    height: 500,
+    height: 600,
     slots: 5,
     walls: {thickness: 10, colour: '#1c75bc'},
+    padding: 0.06,
+    triangles: 5,
     background: '#00205c',
     engine: Matter.Engine.create(),
     canvas: document.getElementById('price-drop'),
@@ -25,97 +26,113 @@ const priceDrop = {
         }
         });
     },
-    buildWalls: function (){
-        const walls = [Matter.Bodies.rectangle(
-                this.width/2, 
-                this.height - this.walls.thickness/2, 
-                this.width, 
-                this.walls.thickness, {
-            render: { fillStyle: this.walls.colour},
-            isStatic: true,
-            }),
-        Matter.Bodies.rectangle(
-                this.walls.thickness/2, 
-                this.height/2, 
-                this.walls.thickness, 
-                this.height, {
-            render: { fillStyle: this.walls.colour},
-            isStatic: true,
-            }),
-        Matter.Bodies.rectangle(
-                this.width - this.walls.thickness/2, 
-                this.height/2, 
-                this.walls.thickness, 
-                this.height, {
-            render: { fillStyle: this.walls.colour},
-            isStatic: true,
-            })
+    buildWalls: function () {
+        const walls = [];
+        // Create side walls and floor
+        const leftWall = Matter.Bodies.rectangle(
+            this.walls.thickness/2,
+            this.height/2,
+            this.walls.thickness,
+            this.height, {
+                render: { fillStyle: this.walls.colour },
+                isStatic: true
+            }
+        );
+        const rightWall = Matter.Bodies.rectangle(
+            this.width-(this.walls.thickness/2),
+            this.height/2,
+            this.walls.thickness,
+            this.height, {
+                render: { fillStyle: this.walls.colour },
+                isStatic: true
+            }
+        );
+        const floor = Matter.Bodies.rectangle(
+            this.width/2,
+            this.height-(this.walls.thickness/2),
+            this.width,
+            this.walls.thickness, {
+                render: { fillStyle: this.walls.colour },
+                isStatic: true
+            }
+        );
+        
+        // Create triangles along left wall
+        const padding = this.width*this.padding;
+        const triangleHeight = this.height*0.16;
+        let y = (triangleHeight/2)+(this.height-triangleHeight*this.triangles)-this.walls.thickness-triangleHeight/2;
+        const verticesLeft = [
+            { x: 0, y: 0 },
+            { x: padding, y: triangleHeight/2 },
+            { x: 0, y: triangleHeight }
         ];
- 
-        let triangleRadius = this.width*0.06;
-        // Draw triangles down left side
-        let leftX = triangleRadius/2;
-        let leftY = this.height*0.17/2 + this.height*0.09;
-        for (let i=0; i < 5; i++){
-            const triangle = Matter.Bodies.polygon(
-                leftX, leftY, 3, triangleRadius, {
-                    render: { fillStyle: this.walls.colour },
-                    isStatic: true
+        const verticesRight = [
+            { x: 0, y: triangleHeight/2 },
+            { x: padding, y: 0 },
+            { x: padding, y: triangleHeight }
+        ];
+        
+        // Calculate x value of centroids
+        const centerXPosLeft = (verticesLeft[0].x+verticesLeft[1].x+verticesLeft[2].x)/3;
+        const centerXPosRight = (verticesRight[0].x+verticesRight[1].x+verticesRight[2].x)/3;
+
+        for (let i=0; i<this.triangles; i++){
+            const triangleLeft = Matter.Bodies.fromVertices(
+                centerXPosLeft+this.walls.thickness,
+                y,
+                verticesLeft, {
+                render: { fillStyle: 'yellow'},
+                isStatic: true
                 }
             );
-            // Stretch and rotate triangle
-            Matter.Body.rotate(triangle, Math.PI / 1);
-            Matter.Body.scale(triangle, 1, (this.height/5)/triangleRadius);
-            walls.push(triangle);
-            leftY = leftY + this.height*0.17;
-        }
-
-        // Draw triangles down right side
-        let rightX = this.width - triangleRadius/2;
-        let rightY = this.height*0.17/2 + this.height*0.09;
-        for (let i=0; i < 5; i++){
-            const triangle = Matter.Bodies.polygon(
-                rightX, rightY, 3, triangleRadius, {
-                    render: { fillStyle: this.walls.colour },
-                    isStatic: true
+            const triangleRight = Matter.Bodies.fromVertices(
+                this.width-centerXPosRight/2-this.walls.thickness,
+                y,
+                verticesRight, {
+                render: { fillStyle: 'green'},
+                isStatic: true
                 }
             );
-            // Stretch triangle
-            Matter.Body.scale(triangle, 1, (this.height/5)/triangleRadius);
-            walls.push(triangle);
-            rightY = rightY + this.height*0.17;
+            walls.push(triangleLeft, triangleRight);
+            y = y + triangleHeight;
         }
 
-        // Draw bottom wall blocks
-        const bottomBlockHeight = this.height-((this.height*0.17)*5);
-        const leftBlock = Matter.Bodies.rectangle(
-            this.width*0.09/2, 
-            this.height-(this.height*0.17/2)+this.walls.thickness, 
-            this.width*0.09, 
-            bottomBlockHeight, { 
-                render: { fillStyle: this.walls.colour }, 
+        // Add side walls to slots area
+        const slotWallWidth = padding;
+        const slotWallHeight = triangleHeight;
+        const slotWallLeft = Matter.Bodies.rectangle(
+            padding/2+this.walls.thickness,
+            this.height-(triangleHeight/2)-this.walls.thickness,
+            slotWallWidth,
+            slotWallHeight, {
+                render: { fillStyle: '#c007'},
                 isStatic: true
-        });
-        const rightBlock = Matter.Bodies.rectangle(
-            this.width-this.width*0.09/2, 
-            this.height-(this.height*0.17/2)+this.walls.thickness, 
-            this.width*0.09, 
-            bottomBlockHeight, { 
-                render: { fillStyle: this.walls.colour }, 
+            }
+
+        );
+        const slotWallRight = Matter.Bodies.rectangle(
+            this.width-(padding/2+this.walls.thickness),
+            this.height-(triangleHeight/2)-this.walls.thickness,
+            slotWallWidth,
+            slotWallHeight, {
+                render: { fillStyle: '#c007'},
                 isStatic: true
-        });
-        walls.push(leftBlock);
-        walls.push(rightBlock);
+            }
+
+        );
+        
+        walls.push(leftWall, rightWall, floor, slotWallLeft, slotWallRight);
         return walls;
     },
     buildPegs: function() {
         const blockHeight = this.height-((this.height*0.17)*5);
+        const totalWidth = (this.width-((this.width*0.09)*2));
         const pegs = [];
         const rows = 10;
         const pegRadius = (((this.width-((this.width*0.09)*2))/this.slots)/30)
-        let pegGapX = (this.width-(this.width*0.18))/this.slots;
+        let pegGapX = totalWidth/this.slots;
         let pegGapY = (this.height*0.17)/2;
-        let x = pegGapX;
+        let x = totalWidth-(pegGapX*(this.slots))+(this.width*0.09)+(pegGapX/2);
         let y = this.height-(pegGapY*rows)-blockHeight/2.75;
         
         for (let row=0; row < rows; row++){
@@ -155,8 +172,7 @@ const priceDrop = {
         const dividers = [];
         const totalWidth = (this.width-((this.width*0.09)*2));
         const height = 40;
-        const dividerGap = totalWidth/this.slots+(this.walls.thickness*(this.slots-1));
-        console.log(dividerGap);
+        const dividerGap = totalWidth/this.slots+(this.walls.thickness/(this.slots-1));
         let x = (this.width*0.09)+dividerGap-(this.walls.thickness/2);
         let y = this.height-(height/2)-this.walls.thickness;
         for (let i=0; i < this.slots-1; i++) {
@@ -182,6 +198,7 @@ const priceDrop = {
     Matter.Composite.add(priceDrop.engine.world, priceDrop.buildPegs());
     Matter.Composite.add(priceDrop.engine.world, priceDrop.buildSlotDividers());
     Matter.Composite.add(priceDrop.engine.world, priceDrop.buildDisc(88));
+
 
 
     const runner = Matter.Runner.create();
